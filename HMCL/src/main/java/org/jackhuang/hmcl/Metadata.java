e6c5cd172046b87cd474984813438c7aaff2d986
NilAnswer <150058714+NilAnswer@users.noreply.github.com>
2024-01-06 18:13:56 +0800

    设置版本

--- HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
@@ -32,7 +32,7 @@ public final class Metadata {
 
     public static final String NAME = "HMCL";
     public static final String FULL_NAME = "Hello Minecraft! Launcher";
-    public static final String VERSION = System.getProperty("hmcl.version.override", JarUtils.getManifestAttribute("Implementation-Version", "3.5.5"));
+    public static final String VERSION = System.getProperty("hmcl.version.override", "3.5.5");
 
     public static final String TITLE = NAME + " " + VERSION;
     public static final String FULL_TITLE = FULL_NAME + " v" + VERSION;
